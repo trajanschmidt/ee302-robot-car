f6fbc926f6bb4a5efc4f0aa3c12f763c3a878a34
johnluetz <43858566+johnluetz@users.noreply.github.com>
2018-12-09 14:25:53 -0600

    the correct code

--- car_code_v2.ino
+++ car_code_v2.ino
@@ -1,10 +1,10 @@
 //L293D
-//Motor A
-const int motorPin1  = 5;  // Pin 14 of L293
-const int motorPin2  = 6;  // Pin 10 of L293
-//Motor B
-const int motorPin3  = 10; // Pin  7 of L293
-const int motorPin4  = 9;  // Pin  2 of L293
+//Motor R
+const int forwardR  = 5;  // Pin 14 of L293
+const int backwardR  = 6;  // Pin 10 of L293
+//Motor L
+const int backwardL  = 10; // Pin  7 of L293
+const int forwardL  = 9;  // Pin  2 of L293
 
 int lightLeft, highL = 0, lowL = 1023;
 int lightCenter, highC = 0, lowC = 1023;
@@ -12,21 +12,29 @@ int lightRight, highR = 0, lowR = 1023;
 int phase = 0;
 
 const int leftSensor = A5;
-const int centerSensor = A4;
+const int centerSensor = A4;  //input sensors
 const int rightSensor = A3;
+
+const int buzzer = 13; //debug beeps!
 
 
 
 //This will run only one time.
 void setup(){
     //Set pins as outputs
-    pinMode(motorPin1, OUTPUT);
-    pinMode(motorPin2, OUTPUT);
-    pinMode(motorPin3, OUTPUT);
-    pinMode(motorPin4, OUTPUT);
-    //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
-    //This code  will turn Motor A clockwise for 2 sec.
+    pinMode(forwardR, OUTPUT);
+    pinMode(backwardR, OUTPUT);
+    pinMode(backwardL, OUTPUT);
+    pinMode(forwardL, OUTPUT);
+
+   pinMode(forwardL, LOW);
+   pinMode(forwardR, LOW);
+   pinMode(backwardR, LOW);
+   pinMode(backwardL, LOW);
+    pinMode(buzzer, OUTPUT); 
     Serial.begin(9600);
+
+
 }
 void loop(){
 
@@ -34,20 +42,67 @@ void loop(){
   lightCenter = analogRead(centerSensor);
   lightRight = analogRead(rightSensor);
 
-
+ 
+  lightLeft = map(lightLeft, 100, 500, 0, 255);
+  lightRight = map(lightRight, 20, 100, 0, 255);
+  lightCenter = map(lightCenter, 505, 909, 0, 255); 
+   
+  lightLeft = constrain(lightLeft, 0, 255);
+  lightCenter = constrain(lightCenter, 0, 255);
+  lightRight = constrain(lightRight, 0, 255);
   //manualTune();  // manually change the range from light to dark
 
   //analogWrite(ledPin, lightLevel);
 
-  Serial.println('\n');
-  Serial.println("Center");
-  Serial.println(lightCenter);
-  Serial.println("Left");
+  Serial.println("Values:");
   Serial.println(lightLeft);
-  Serial.println("Right");
+  Serial.println(lightCenter);
   Serial.println(lightRight);
 
-  delay(1000);
+  
 
+
+  if(lightLeft<100 && lightRight>100 )  //adjust right
+  {
+   pinMode(forwardL, HIGH);
+   pinMode(forwardR, LOW); 
+   pinMode(backwardL, LOW);
+   //tone(buzzer, 1000);
+   Serial.println("TURN RIGHT!");
+   delay(50);
+  }
+  else if (lightRight<100 && lightLeft>100) //adjust left
+  {
+   pinMode(forwardL, LOW);
+   pinMode(forwardR, HIGH); 
+   //tone(buzzer, 500);
+   Serial.println("TURN LEFT!");
+   delay(50);
+  }
+
+  else if (lightLeft<100 && lightCenter < 100) //90 degree
+  {
+   pinMode(forwardL, HIGH);
+   pinMode(backwardR, LOW); 
+   pinMode(backwardL, LOW);
+    Serial.println("TURN TURN TURN LEFT");
+    pinMode(forwardR, LOW);
+    delay(500);
+  } 
+  else
+  {
+    Serial.println("GO STRAIGHT!");
+    pinMode(forwardL, LOW);
+    pinMode(backwardL, HIGH);
+    pinMode(forwardR, LOW);
+    pinMode(backwardR, HIGH); 
+    //tone(buzzer, 2000);
+    
+  }
+
+  
+
+  
+  
   
 }
